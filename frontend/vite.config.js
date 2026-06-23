196d7d7fb542e5157f36e3d07c864c2a2c5e8ff2
jose_manuel_garcia_del_prado <josemanuel.garcia@nter.es>
2026-06-23 12:08:05 +0200

    ref: nueva config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ProyectoIA/',
+  base: '/proyectoIA-BCC/',
   plugins: [react()],
   server: {
     proxy: {
